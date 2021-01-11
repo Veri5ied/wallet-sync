ae33d0223774c330a2475081fe69b098862ece6e
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-11 10:09:32 +0100

    client and server setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text">
+        Hello World
+      </h2>
+    </div>
+  );
+}
+
+export default App;
