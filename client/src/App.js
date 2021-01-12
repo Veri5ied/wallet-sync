98accdfb37a0c2163caf2c86566df5707feafe99
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-13 00:33:11 +0100

    client's home

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/home/Home";
 
 function App() {
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 function Home() {
-    return (
-        <div>
-            
+  return (
+    <div className="home">
+      <div className="home__hero">
+        <div className="home__title">
+          <h3>
+            The new era of electronic <span>payments</span>
+          </h3>
+          <h4>
+            New level finance product with new functionality that is available
+            to everyone
+          </h4>
         </div>
-    )
+        <div className="home__button">
+          <button className="get__app">Get the app</button>
+          <button className="get__started">Get started</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
