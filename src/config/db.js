f98da8e539c0fcf2bf85418cda7ecf42f59b2536
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-12 01:33:46 +0100

    mongodb setup

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 export const connect = () => {
-    mongoose.connect()
+    mongoose.connect(
+      `mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.1izqg.mongodb.net/${process.env.db_name}?retryWrites=true&w=majority`
+    );
 }
 
