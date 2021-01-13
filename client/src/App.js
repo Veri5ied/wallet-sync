e6a0863ec30537ab012c445544d27d5194a9d708
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2021-01-13 02:13:08 +0100

    loading animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loading from "./components/loading/Loading";
 import Home from "./pages/home/Home";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 5000);
+  }, []);
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </Router>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      )}
+    </>
   );
 }
 
---
+++ client/src/components/loading/Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Loading.css"
+
+function Loading() {
+    return (
+      <div class="container">
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+        <div class="block"></div>
+      </div>
+    );
+}
+
+export default Loading;
